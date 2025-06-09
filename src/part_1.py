7e920121ff299f3b2370fd9909ce4b221b3a5455
lehoanglong95 <lehoanglong95@gmail.com>
2025-06-09 11:38:59 +1000

    part_1

--- src/part_1.py
+++ src/part_1.py
@@ -1,12 +1,15 @@
 # LIST COMPREHENSION
-# create a new list with only odd numbers from arr = [1, 2, 3, 4, 5]
+
+# Assignment: create a new list with only odd numbers from arr = [1, 2, 3, 4, 5]
 
 arr = [1, 2, 3, 4, 5]
 
 # junior
-new_arr = [] # Đầu tiên, tạo một danh sách rỗng
-for e in arr: # Sau đó, dùng vòng lặp để đi qua từng phần tử trong danh sách
-    if e % 2 == 1: # Trong vòng lặp, kiểm tra xem phần tử đó có phải là số lẻ hay không ... Nếu đúng, thì thêm nó vào danh sách mới ...  
+new_arr = []  # Đầu tiên, tạo một danh sách rỗng
+for e in arr:  # Sau đó, dùng vòng lặp để đi qua từng phần tử trong danh sách
+    if (
+        e % 2 == 1
+    ):  # Trong vòng lặp, kiểm tra xem phần tử đó có phải là số lẻ hay không ... Nếu đúng, thì thêm nó vào danh sách mới ...
         new_arr.append(e)
 
 # senior

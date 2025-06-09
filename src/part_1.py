a0af12d7bbc4988b9dd057bf5e80d4f8d0d5b5f2
lehoanglong95 <lehoanglong95@gmail.com>
2025-06-09 22:07:32 +1000

    update part_1

--- src/part_1.py
+++ src/part_1.py
@@ -5,11 +5,9 @@
 arr = [1, 2, 3, 4, 5]
 
 # junior
-new_arr = []  # Đầu tiên, tạo một danh sách rỗng
-for e in arr:  # Sau đó, dùng vòng lặp để đi qua từng phần tử trong danh sách
-    if (
-        e % 2 == 1
-    ):  # Trong vòng lặp, kiểm tra xem phần tử đó có phải là số lẻ hay không ... Nếu đúng, thì thêm nó vào danh sách mới ...
+new_arr = []
+for e in arr:
+    if e % 2 == 1:
         new_arr.append(e)
 
 # senior
